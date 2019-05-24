cb64a4c53cc6dc770602096eb8cc18967af1a547
albertariel2013 <42852258+albertariel2013@users.noreply.github.com>
2019-05-23 20:42:31 -0400

    Numbers In Words part 2

--- 2019-05-22.cs
+++ 2019-05-22.cs
@@ -1,78 +1,157 @@
 ﻿using System;
 
-namespace NumberInWord_P3
+namespace NumbersInWord_p2
 {
     class Program
     {
-        public int Checkfollowing(string[] A, int j)
-        {
-            int i = j+1;
-
-            if (A[i].ToLower() == "hundred")
-            {
-                string.
-            }
-
-
-            return 0;
-        }
         static void Main(string[] args)
         {
-            string TestCase = "Two Thousand One Hundred";
-            string result = " ";
-            string[] words = TestCase.Split(' ');
-            
-            foreach (string number in words)
-            {
-                Console.WriteLine(number);
-            }
+            string conversion = "";
+            string num = "0";
+            int digits = num.Length;
+            char[] Numbers = num.ToCharArray();
 
-            for (int i = 0; i < words.Length; i++)
+            for (int i = 0; i < Numbers.Length; i++)
             {
-                switch (words[i].ToLower())
+                if (digits == 2)
                 {
-                    case "one":
-                        result += "1";
-                        break;
-                    case "two":
-                        result += "2";
-                        break;
-                    case "three":
-                        result += "3";
-                        break;
-                    case "four":
-                        result += "4";
-                        break;
-                    case "five":
-                        result += "5";
-                        break;
-                    case "six":
-                        result += "6";
-                        break;
-                    case "seven":
-                        result += "7";
-                        break;
-                    case "eight":
-                        result += "8";
-                        break;
-                    case "nine":
-                        result += "9";
-                        break;
+                    switch (Numbers[i])
+                    {
+                        case '1':
+                            if (Numbers[i + 1] == '1')
+                            {
+                                conversion += "Eleven ";
+                                
+                            }
+                            else if (Numbers[i + 1] == '2')
+                            {
+                                conversion += "Twelve ";
+
+                            }
+                            else if (Numbers[i + 1] == '3')
+                            {
+                                conversion += "Thirteen ";
+
+                            }
+                            else if (Numbers[i + 1] == '4')
+                            {
+                                conversion += "Fourteen ";
+
+                            }
+                            else if (Numbers[i + 1] == '5')
+                            {
+                                conversion += "Fifteen ";
+
+                            }
+                            else if (Numbers[i + 1] == '6')
+                            {
+                                conversion += "Sixteen ";
+
+                            }
+                            else if (Numbers[i + 1] == '7')
+                            {
+                                conversion += "Seventeen ";
+
+                            }
+                            else if (Numbers[i + 1] == '8')
+                            {
+                                conversion += "Eighteen ";
+
+                            }
+                            else if (Numbers[i + 1] == '9')
+                            {
+                                conversion += "Nineteen ";
+
+                            }
+                            else {
+                                conversion += "Ten ";
+                            }
+                            i = Numbers.Length;
+                            
+                            break;
+                        case '2':
+                            conversion += "Twenty ";
+                            break;
+                        case '3':
+                            conversion += "Thirty ";
+                            break;
+                        case '4':
+                            conversion += "Fourty ";
+                            break;
+                        case '5':
+                            conversion += "Fifty ";
+                            break;
+                        case '6':
+                            conversion += "Sixty ";
+                            break;
+                        case '7':
+                            conversion += "Seventy ";
+                            break;
+                        case '8':
+                            conversion += "Eighty ";
+                            break;
+                        case '9':
+                            conversion += "Ninety ";
+                            break;
+                    }
+                    digits--;
                 }
+                else {
+
+                    switch (Numbers[i])
+                    {
+                        case '1':
+                            conversion += "One ";
+                            break;
+                        case '2':
+                            conversion += "Two ";
+                            break;
+                        case '3':
+                            conversion += "Three ";
+                            break;
+                        case '4':
+                            conversion += "Four ";
+                            break;
+                        case '5':
+                            conversion += "Five ";
+                            break;
+                        case '6':
+                            conversion += "Six ";
+                            break;
+                        case '7':
+                            conversion += "Seven ";
+                            break;
+                        case '8':
+                            conversion += "Eight ";
+                            break;
+                        case '9':
+                            conversion += "Nine ";
+                            break;
+                    }
+                    if (digits == 4)
+                    {
+                        conversion += "Thousand ";
+                    }
+                    else if (digits == 3)
+                    {
+                        conversion += "Hundred ";
+                    }
+                    else if (digits == 1)
+                    {
+                        if (Numbers[i] == 0)
+                        {
+                            conversion += "zero";
+                        }
+                        continue;
+                    }
+                    digits--;
+                }
+                
+                
             }
+            Console.WriteLine(conversion);
+            Console.ReadLine();
 
-            /*
-             * 
-             * 1) convertir el numero dado a un string. Convetir ese string en un arreglo de chars, y luego
-             * descomponerlo para ver a que corresponde cada num
-             * 
-             * 2) descomponer el string con string.split(' ') y luego iterar sobre cada indice del array desde el inicio
-             * Ver a que cifra corresponde y eso
-             * */
-
-
-
-            Console.ReadKey();
         }
     }
 }
