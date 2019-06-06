d2f496db716847668fb0d4347db4e526ba38c83f
albertariel2013 <42852258+albertariel2013@users.noreply.github.com>
2019-06-06 12:51:31 -0400

    String Calculator Part 1

---
+++ 2019-06-03.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StringCalc__p1
+{
+
+  public class StringCalculator
+    {
+        public string Add(string input)
+        {
+            if (input == "")
+            {
+                return "0";
+            }
+            string[] operands = input.Split(',');
+
+            if (operands.Length == 1)
+            {
+                return operands[0];
+            }
+
+            List<int> numbers = new List<int>();
+            //how to add values in strings
+
+            //Pendiente: how to add numbers with two digits
+            for (int i = 0; i < operands.Length; i++)
+            {
+                if (operands[i] == "1")
+                {
+                    numbers.Add(1);
+                }
+                if (operands[i] == "2")
+                {
+                    numbers.Add(2);
+                }
+                if (operands[i] == "3")
+                {
+                    numbers.Add(3);
+                }
+                if (operands[i] == "4")
+                {
+                    numbers.Add(4);
+                }
+                if (operands[i] == "5")
+                {
+                    numbers.Add(5);
+                }
+                if (operands[i] == "6")
+                {
+                    numbers.Add(6);
+                }
+                if (operands[i] == "7")
+                {
+                    numbers.Add(7);
+                }
+                if (operands[i] == "8")
+                {
+                    numbers.Add(8);
+                }
+                if (operands[i] == "9")
+                {
+                    numbers.Add(9);
+                }
+            }
+
+            int sum = 0;
+            foreach (int numbr in numbers)
+                sum += numbr;
+            return sum.ToString();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 2019-06-06 Testcode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using StringCalc__p1;
+
+namespace Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        StringCalculator calculator = new StringCalculator();
+        [Test]
+
+        public void EmptyString()
+        {
+            
+            string teststring = "";
+            string output = calculator.Add(teststring);
+
+            Assert.AreEqual("0", output);
+        }
+
+        [Test]
+
+        public void One_Number_Test()
+        {
+            string teststring = "1";
+            string output = calculator.Add(teststring);
+
+            Assert.AreEqual("1", output);
+        }
+
+        [Test]
+
+        public void Two_Number_Test_part1()
+        {
+            string teststring = "1,2";
+            string output = calculator.Add(teststring);
+
+            Assert.AreEqual("3", output);
+        }
+
+        [Test]
+        
+        public void Two_Number_Test_twodigits()
+        {
+            string teststring = "10,22";
+            string output = calculator.Add(teststring);
+
+            Assert.AreEqual("32", output);
+        }
+    }
+}
