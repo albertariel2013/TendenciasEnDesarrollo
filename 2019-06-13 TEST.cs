fbbbd568a4107384873fb8cb223e0ea9f733cb2b
albertariel2013 <42852258+albertariel2013@users.noreply.github.com>
2019-06-15 19:07:16 -0400

    2019-06-13
    Sorting It Out P1

---
+++ 2019-06-13 TEST.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Kata_S7_p1;
+using System.Collections.Generic;
+namespace Test_Unit
+{
+    [TestFixture]
+    public class Class1
+    {
+        Rack rack = new Rack();
+        [Test]
+
+        public void One_Number_test()
+        {
+           
+            int[] result = rack.Add(10);
+            int[] expected = { 10 };
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Second_Number_test()
+        {
+            int[] result = rack.Add(20);
+            int[] expected = { 10, 20 };
+            Assert.AreEqual(expected, result);
+        }
+        
+        [Test]
+        public void Third_Number_test()
+        {
+            int[] result = rack.Add(50);
+            int[] expected = { 5,10,20 };
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
