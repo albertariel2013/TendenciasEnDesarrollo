2b745c3cf1f988e956aaddae5dad1907bbef56fe
albertariel2013 <42852258+albertariel2013@users.noreply.github.com>
2019-06-23 23:38:24 -0400

    2019-06-18

---
+++ 2019-06-18 TEST.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_calc_p2
+{
+
+
+    public class Container
+    {
+        public bool Contains(string interval, string input)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ 2019-06-18.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using string_calc_p2;
+using System.Collections.Generic;
+namespace Test
+{
+    [TestFixture]
+    public class Class2
+    {
+
+        Container container = new Container();
+
+        [Test]
+
+        public void Test1_2to6_Contains2to4()
+        {
+            bool Result = container.contains("[2,6)", "[2,4]");
+
+            Assert.AreEqual(Result, true);
+        }
+    }
+}
