d21ed8d1110cf15cf42570a59390bf2db1e7deb9
albertariel2013 <42852258+albertariel2013@users.noreply.github.com>
2019-06-23 23:39:29 -0400

    2019-06-23
    Range Part 2

---
+++ 2019-06-23 TEST.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Range_p2;
+using System.Collections.Generic;
+namespace Test
+{
+    
+    public class Class1
+    {
+
+
+        Container container = new Container();
+
+
+       [Test]
+
+       public void TwoSixContainsTwoFour()
+       {
+            bool result = container.Contains("[2,6)", "[2,4]");
+            Assert.AreEqual(true, result);
+       }
+    }
+}
---
+++ 2019-06-23.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Range_p2
+{
+
+    public class Container
+    {
+        public bool Contains(string Interval, string input1)
+        {
+            bool result = false;
+            List<int> Elements = new List<int>();
+            char[] CArray = Interval.ToCharArray();
+            char[] CArray2 = input1.ToCharArray();
+
+            char FirstDigit = CArray[1];
+            char LastDigit = CArray[CArray.Length - 1];
+            int LastNumber1;
+
+            if(LastDigit == ')')
+            {
+                LastNumber1 = LastDigit - '0';
+                LastNumber1 -= 1;
+            }
+
+            else    //lastnumber  = ]
+            {
+                LastNumber1 = LastDigit - '0';
+                
+            }
+
+            char FirstDigit2 = CArray2[1];
+            char LastDigit2 = CArray2[CArray2.Length - 1];
+            int LastNumber2;
+
+            if (LastDigit2 == ')')
+            {
+                LastNumber2 = LastDigit - '0';
+                LastNumber2 -= 1;
+            }
+
+            else    //lastnumber  = ]
+            {
+                LastNumber2 = LastDigit - '0';
+
+            }
+
+
+            for(int i = FirstDigit; i <= LastNumber1; i++)
+            {
+                Elements.Add(i);
+            }
+
+            if (Elements.Contains(LastNumber2))
+                result = true;
+
+            return result;
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Container container = new Container();
+            bool result = container.Contains("[2,6)", "[2,4]");
+        }
+    }
+}
