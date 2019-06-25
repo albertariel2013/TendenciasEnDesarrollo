7f1655ebe76f92543f9b6ba1cb2491ab3dc36a7b
albertariel2013 <42852258+albertariel2013@users.noreply.github.com>
2019-06-25 12:30:21 -0400

    Word Wrap Part 1

---
+++ 2019-06-25 TEST.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Word_Wrap_p1;
+
+namespace Test
+{
+
+    [TestFixture]
+
+    public class Class1
+    {
+        Wrapper wrapper = new Wrapper();
+        
+        [Test]
+
+        public void Test1_SimpleWordWithLength2()
+        {
+
+            string expected = "a s";
+            string result = wrapper.Wrap("as", 1);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+
+
+        /*
+         * I need to make sure that no word in the input is longer than a column limit
+         * if so, need to place a \n inside of it.
+         * 
+         * how:
+         * Count how many digits between a \n and another \n. If the word surpasses the limits
+         * place a \n on it. if not, let it be.
+         * 
+         * */
+        public void Test2_2WordsLength5()
+        {
+            string expected = "hello world";
+            string result = wrapper.Wrap("hello world", 7);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+
+    }
+}
---
+++ 2019-06-25.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Word_Wrap_p1
+{
+
+    public class Wrapper
+    {
+
+        public string Wrap (string input, int idx)
+        {
+            string word1 = input;
+            int counter = 0;
+            string newstring = input;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (word1[i] == '\n')
+                {
+                    if (counter >= idx)
+                    {
+                        newstring = input.Insert(idx, " ");
+                    }
+                    counter = 0;
+                }
+                else
+                {
+                    counter += 1;
+                }
+                
+            }
+
+
+             
+
+            return newstring;
+            
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
