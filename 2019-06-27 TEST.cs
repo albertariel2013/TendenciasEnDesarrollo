c391e610904eaeb488180eebc417e30a87ca35c6
albertariel2013 <42852258+albertariel2013@users.noreply.github.com>
2019-06-27 17:28:32 -0400

    Word Wrap Part 2

---
+++ 2019-06-27 TEST.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using word_wrap_p2;
+namespace Test
+{
+
+
+    [TestFixture]
+    public class Class1
+    {
+        Wrapper wrapper = new Wrapper();
+
+        [Test]
+
+        public void Split2DigitsWordIn1()
+        {
+            string testinput = "bf";
+            string result = wrapper.Wrap(testinput, 1);
+            Assert.AreEqual("b f", result);
+        }
+
+        [Test]
+
+        public void Split2WordsifLengthMoreThanThree()
+        {
+
+            string testinput = "Hello World";
+            string result = wrapper.Wrap(testinput, 3);
+            Assert.AreEqual("Hel lo Wor ld", result);
+
+
+        }
+
+        [Test]
+        public void Split3WordsifLengthMoreThan10()
+        {
+            string testinput = "Hello World asadersa";
+            string result = wrapper.Wrap(testinput, 10);
+            Assert.AreEqual("Hello World asadersa", result);
+        }
+
+
+        /*
+         * 
+         * ESTO HA SIDO TODO POR LA KATA DE HOY, NOS VIMOS LA SEMANA SIGUIENTE!
+         * */
+    }
+}
---
+++ 2019-06-27.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace word_wrap_p2
+{
+
+    public class Wrapper
+    {
+
+        public string Wrap(string input, int pos)
+        {
+
+            //Problem is how to count words in a string
+            
+            int counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if(input[i] == ' ')
+                {
+                    counter = 0;
+                }
+
+                else
+                {
+                    if(counter >= pos)
+                    {
+                        input = input.Insert(i, " ");
+                        counter = -1;
+                    }
+                      counter++;
+
+                }
+            }
+
+            return input;
+
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Wrapper newwrap = new Wrapper();
+            //newwrap.Wrap("bf", 1);
+
+            //test2 porque se bugeo mi nunit
+
+            Console.WriteLine(newwrap.Wrap("Hello World", 3));
+        }
+    }
+}
